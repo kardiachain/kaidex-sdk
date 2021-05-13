a3e5926ca2249a609d4588a04910f5125ba9bdd6
phidangnguyen <phidang0210@gmail.com>
2021-05-13 11:54:42 +0700

    Export krc20 service

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { KaidexClient } from './entities/kaidex-client';
-
 export { Fraction } from './entities/fraction';
 export { Utils } from './utils';
+export { KRC20Service } from './services/krc20'
 
 export default KaidexClient;
